b81ba67f94fe1d87fe33be65010e6d87b6b03b80
SiedzeNaKrzesle <micekwiktor@gmail.com>
2025-04-06 02:04:56 +0200

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,18 +1,14 @@
-﻿
-
+﻿// Form1.Designer.cs
 namespace PROJOP_AP
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox ekran;
+        private System.Windows.Forms.TableLayoutPanel panel;
+        private System.Windows.Forms.Button[] cyfry = new System.Windows.Forms.Button[10];
+        private System.Windows.Forms.Button plus, minus, razy, dziel, rowna, clear;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,279 +18,93 @@ namespace PROJOP_AP
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            ekran = new TextBox();
-            contextMenuStrip1 = new ContextMenuStrip(components);
-            guzik1 = new Button();
-            guzik2 = new Button();
-            guzik3 = new Button();
-            guzik5 = new Button();
-            guzik4 = new Button();
-            guzik8 = new Button();
-            guzik9 = new Button();
-            guzik0 = new Button();
-            guzik7 = new Button();
-            guzik6 = new Button();
-            sigma_razy = new Button();
-            sigma_dziel = new Button();
-            wyczysc = new Button();
-            sigma_minus = new Button();
-            sigma_plus = new Button();
-            sigma_rowna = new Button();
-            SuspendLayout();
-            // 
+            this.ekran = new System.Windows.Forms.TextBox();
+            this.panel = new System.Windows.Forms.TableLayoutPanel();
+
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.razy = new System.Windows.Forms.Button();
+            this.dziel = new System.Windows.Forms.Button();
+            this.rowna = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+
+            // Form
+            this.SuspendLayout();
+            this.ClientSize = new System.Drawing.Size(400, 600);
+            this.Name = "Form1";
+            this.Text = "Kalkulator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
             // ekran
-            // 
-            ekran.Font = new Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            ekran.Location = new Point(12, 12);
-            ekran.Multiline = true;
-            ekran.Name = "ekran";
-            ekran.Size = new Size(299, 55);
-            ekran.TabIndex = 0;
-            ekran.TextChanged += ekran_TextChanged;
-            // 
-            // contextMenuStrip1
-            // 
-            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
-            contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(61, 4);
-            // 
-            // guzik1
-            // 
-            guzik1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik1.Location = new Point(17, 73);
-            guzik1.Name = "guzik1";
-            guzik1.Size = new Size(94, 41);
-            guzik1.TabIndex = 2;
-            guzik1.Text = "1";
-            guzik1.UseVisualStyleBackColor = true;
-            guzik1.Click += guzik1_Click;
-            // 
-            // guzik2
-            // 
-            guzik2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik2.Location = new Point(117, 73);
-            guzik2.Name = "guzik2";
-            guzik2.Size = new Size(94, 41);
-            guzik2.TabIndex = 3;
-            guzik2.Text = "2";
-            guzik2.UseVisualStyleBackColor = true;
-            guzik2.Click += guzik2_Click;
-            // 
-            // guzik3
-            // 
-            guzik3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik3.Location = new Point(217, 73);
-            guzik3.Name = "guzik3";
-            guzik3.Size = new Size(94, 41);
-            guzik3.TabIndex = 8;
-            guzik3.Text = "3";
-            guzik3.UseVisualStyleBackColor = true;
-            guzik3.Click += guzik3_Click;
-            // 
-            // guzik5
-            // 
-            guzik5.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik5.Location = new Point(117, 120);
-            guzik5.Name = "guzik5";
-            guzik5.Size = new Size(94, 41);
-            guzik5.TabIndex = 6;
-            guzik5.Text = "5";
-            guzik5.UseVisualStyleBackColor = true;
-            guzik5.Click += guzik5_Click;
-            // 
-            // guzik4
-            // 
-            guzik4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik4.Location = new Point(17, 120);
-            guzik4.Name = "guzik4";
-            guzik4.Size = new Size(94, 41);
-            guzik4.TabIndex = 7;
-            guzik4.Text = "4";
-            guzik4.UseVisualStyleBackColor = true;
-            guzik4.Click += guzik4_Click;
-            // 
-            // guzik8
-            // 
-            guzik8.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik8.Location = new Point(117, 167);
-            guzik8.Name = "guzik8";
-            guzik8.Size = new Size(94, 41);
-            guzik8.TabIndex = 13;
-            guzik8.Text = "8";
-            guzik8.UseVisualStyleBackColor = true;
-            guzik8.Click += guzik8_Click;
-            // 
-            // guzik9
-            // 
-            guzik9.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik9.Location = new Point(217, 167);
-            guzik9.Name = "guzik9";
-            guzik9.Size = new Size(94, 41);
-            guzik9.TabIndex = 12;
-            guzik9.Text = "9";
-            guzik9.UseVisualStyleBackColor = true;
-            guzik9.Click += guzik9_Click;
-            // 
-            // guzik0
-            // 
-            guzik0.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik0.Location = new Point(117, 214);
-            guzik0.Name = "guzik0";
-            guzik0.Size = new Size(94, 41);
-            guzik0.TabIndex = 11;
-            guzik0.Text = "0";
-            guzik0.UseVisualStyleBackColor = true;
-            guzik0.Click += guzik0_Click;
-            // 
-            // guzik7
-            // 
-            guzik7.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik7.Location = new Point(17, 167);
-            guzik7.Name = "guzik7";
-            guzik7.Size = new Size(94, 41);
-            guzik7.TabIndex = 10;
-            guzik7.Text = "7";
-            guzik7.UseVisualStyleBackColor = true;
-            guzik7.Click += guzik7_Click;
-            // 
-            // guzik6
-            // 
-            guzik6.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            guzik6.Location = new Point(217, 120);
-            guzik6.Name = "guzik6";
-            guzik6.Size = new Size(94, 41);
-            guzik6.TabIndex = 9;
-            guzik6.Text = "6";
-            guzik6.UseVisualStyleBackColor = true;
-            guzik6.Click += guzik6_Click;
-            // 
-            // sigma_razy
-            // 
-            sigma_razy.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            sigma_razy.Location = new Point(17, 261);
-            sigma_razy.Name = "sigma_razy";
-            sigma_razy.Size = new Size(94, 41);
-            sigma_razy.TabIndex = 18;
-            sigma_razy.Text = "*";
-            sigma_razy.UseVisualStyleBackColor = true;
-            sigma_razy.Click += sigma_razy_Click;
-            // 
-            // sigma_dziel
-            // 
-            sigma_dziel.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            sigma_dziel.Location = new Point(117, 261);
-            sigma_dziel.Name = "sigma_dziel";
-            sigma_dziel.Size = new Size(94, 41);
-            sigma_dziel.TabIndex = 17;
-            sigma_dziel.Text = "/";
-            sigma_dziel.UseVisualStyleBackColor = true;
-            sigma_dziel.Click += sigma_dziel_Click;
-            // 
-            // wyczysc
-            // 
-            wyczysc.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            wyczysc.Location = new Point(214, 261);
-            wyczysc.Name = "wyczysc";
-            wyczysc.Size = new Size(94, 41);
-            wyczysc.TabIndex = 16;
-            wyczysc.Text = "Clear";
-            wyczysc.UseVisualStyleBackColor = true;
-            wyczysc.Click += wyczysc_Click;
-            // 
-            // sigma_minus
-            // 
-            sigma_minus.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            sigma_minus.Location = new Point(217, 214);
-            sigma_minus.Name = "sigma_minus";
-            sigma_minus.Size = new Size(94, 41);
-            sigma_minus.TabIndex = 15;
-            sigma_minus.Text = "-";
-            sigma_minus.UseVisualStyleBackColor = true;
-            sigma_minus.Click += sigma_minus_Click;
-            // 
-            // sigma_plus
-            // 
-            sigma_plus.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            sigma_plus.Location = new Point(17, 214);
-            sigma_plus.Name = "sigma_plus";
-            sigma_plus.Size = new Size(94, 41);
-            sigma_plus.TabIndex = 14;
-            sigma_plus.Text = "+";
-            sigma_plus.UseVisualStyleBackColor = true;
-            sigma_plus.Click += sigma_plus_Click;
-            // 
-            // sigma_rowna
-            // 
-            sigma_rowna.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 238);
-            sigma_rowna.Location = new Point(12, 308);
-            sigma_rowna.Name = "sigma_rowna";
-            sigma_rowna.Size = new Size(299, 41);
-            sigma_rowna.TabIndex = 19;
-            sigma_rowna.Text = "=";
-            sigma_rowna.UseVisualStyleBackColor = true;
-            sigma_rowna.Click += sigma_rowna_Click;
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(320, 359);
-            Controls.Add(sigma_rowna);
-            Controls.Add(sigma_razy);
-            Controls.Add(sigma_dziel);
-            Controls.Add(wyczysc);
-            Controls.Add(sigma_minus);
-            Controls.Add(sigma_plus);
-            Controls.Add(guzik8);
-            Controls.Add(guzik9);
-            Controls.Add(guzik0);
-            Controls.Add(guzik7);
-            Controls.Add(guzik6);
-            Controls.Add(guzik3);
-            Controls.Add(guzik4);
-            Controls.Add(guzik5);
-            Controls.Add(guzik2);
-            Controls.Add(guzik1);
-            Controls.Add(ekran);
-            Name = "Form1";
-            Text = " Kalkulator";
-            Load += Form1_Load;
-            ResumeLayout(false);
-            PerformLayout();
+            this.ekran.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ekran.Font = new System.Drawing.Font("Consolas", 28F);
+            this.ekran.ReadOnly = true;
+            this.ekran.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.ekran.Height = 80;
+            this.Controls.Add(this.ekran);
+
+            // panel
+            this.panel.ColumnCount = 4;
+            this.panel.RowCount = 5;
+            this.panel.Dock = System.Windows.Forms.DockStyle.None;
+            this.panel.Location = new System.Drawing.Point(10, 100); // przesunięcie w dół
+            this.panel.Size = new System.Drawing.Size(380, 480);
+            this.panel.Padding = new Padding(10);
+            this.panel.Margin = new Padding(10);
+            for (int i = 0; i < 4; i++)
+                this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            for (int i = 0; i < 5; i++)
+                this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.Controls.Add(this.panel);
+
+            // cyfry 1-9
+            int num = 1;
+            for (int row = 2; row >= 0; row--)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    cyfry[num] = new System.Windows.Forms.Button();
+                    cyfry[num].Text = num.ToString();
+                    cyfry[num].Dock = DockStyle.Fill;
+                    cyfry[num].Click += new System.EventHandler(this.Cyfra_Click);
+                    this.panel.Controls.Add(cyfry[num], col, row);
+                    num++;
+                }
+            }
+
+
+            cyfry[0] = new System.Windows.Forms.Button();
+            cyfry[0].Text = "0";
+            cyfry[0].Dock = DockStyle.Fill;
+            cyfry[0].Click += new System.EventHandler(this.Cyfra_Click);
+            this.panel.Controls.Add(cyfry[0], 1, 3);
+
+
+            plus.Text = "+";
+            minus.Text = "-";
+            razy.Text = "*";
+            dziel.Text = "/";
+            rowna.Text = "=";
+            clear.Text = "C";
+
+            Button[] operatory = { plus, minus, razy, dziel, rowna, clear };
+            foreach (Button op in operatory)
+            {
+                op.Dock = DockStyle.Fill;
+                op.Click += new System.EventHandler(this.Operator_Click);
+            }
+
+            this.panel.Controls.Add(plus, 3, 0);
+            this.panel.Controls.Add(minus, 3, 1);
+            this.panel.Controls.Add(razy, 3, 2);
+            this.panel.Controls.Add(dziel, 3, 3);
+            this.panel.Controls.Add(rowna, 2, 3);
+            this.panel.Controls.Add(clear, 0, 3);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-
-
-
-
-
-        #endregion
-
-        private TextBox ekran;
-        private ContextMenuStrip contextMenuStrip1;
-        private Button guzik1;
-        private Button guzik2;
-        private Button guzik3;
-        private Button guzik5;
-        private Button guzik4;
-        private Button guzik8;
-        private Button guzik9;
-        private Button guzik0;
-        private Button guzik7;
-        private Button guzik6;
-        private Button sigma_razy;
-        private Button sigma_dziel;
-        private Button wyczysc;
-        private Button sigma_minus;
-        private Button sigma_plus;
-        private Button sigma_rowna;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,16 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace PROJOP_AP
 {
     public partial class Form1 : Form
     {
         string currentInput = "";
-
         string operation = "";
         bool operationPending = false;
+        double result = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -13,108 +18,75 @@ namespace PROJOP_AP
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.BackColor = Color.FromArgb(30, 30, 30);
+            ekran.BackColor = Color.Black;
+            ekran.ForeColor = Color.Lime;
+            ekran.Font = new Font("Consolas", 24);
+            ekran.Text = "0";
 
+            foreach (Control ctrl in panel.Controls)
+            {
+                if (ctrl is Button btn)
+                {
+                    btn.BackColor = Color.FromArgb(50, 50, 50);
+                    btn.ForeColor = Color.DeepSkyBlue;
+                    btn.FlatStyle = FlatStyle.Flat;
+                    btn.FlatAppearance.BorderSize = 0;
+                    btn.Font = new Font("Segoe UI", 22, FontStyle.Bold);
+                    btn.Margin = new Padding(5);
+                }
+            }
         }
 
-        private void ekran_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-
-        private void guzik1_Click(object sender, EventArgs e)
-        {
-           
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void guzik2_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void guzik3_Click(object sender, EventArgs e)
+        private void Cyfra_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             currentInput += button.Text;
             ekran.Text = currentInput;
         }
 
-        private void guzik4_Click(object sender, EventArgs e)
+        private void Operator_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
+            string op = button.Text;
+
+            if (op == "C")
+            {
+                currentInput = "";
+                result = 0;
+                operation = "";
+                operationPending = false;
+                ekran.Text = "0";
+                return;
+            }
+
+            if (op == "=")
+            {
+                Evaluate();
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(currentInput))
+            {
+                result = double.Parse(currentInput);
+                currentInput = "";
+                operation = op;
+                operationPending = true;
+                ekran.Text = "";
+            }
         }
 
-        private void guzik5_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void guzik6_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void guzik7_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void guzik8_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void guzik9_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void guzik0_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        double result = 0;
         private void Evaluate()
         {
-            if (operationPending)
+            if (operationPending && !string.IsNullOrEmpty(currentInput))
             {
                 double secondNum = double.Parse(currentInput);
 
                 switch (operation)
                 {
-                    case "+": 
-                        result += secondNum; 
-                        break;
-
-                    case "-":
-                        result -= secondNum;
-                        break;
-
-                    case "*":
-                        result *= secondNum;
-                        break;
-
-                    case ":":
+                    case "+": result += secondNum; break;
+                    case "-": result -= secondNum; break;
+                    case "*": result *= secondNum; break;
                     case "/":
                         if (secondNum != 0)
                         {
@@ -122,72 +94,16 @@ namespace PROJOP_AP
                         }
                         else
                         {
-                            ekran.Text = "B³¹d";
+                            ekran.Text = "Błąd";
                             return;
                         }
                         break;
-
-
                 }
 
                 ekran.Text = result.ToString();
                 currentInput = "";
                 operationPending = false;
-
-
-
-
-
             }
-
-
-        }
-        private void sigma_plus_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-            
-        private void sigma_razy_Click(object sender, EventArgs e) //metoda od mno¿enia
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void sigma_minus_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void sigma_dziel_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            currentInput += button.Text;
-            ekran.Text = currentInput;
-        }
-
-        private void sigma_rowna_Click(object sender, EventArgs e)
-        {
-            System.Data.DataTable table = new System.Data.DataTable();
-            table.Columns.Add("expression", string.Empty.GetType(), ekran.Text);
-            System.Data.DataRow row = table.NewRow();
-            table.Rows.Add(row);
-            double result = double.Parse((string)row["expression"]);
-
-            ekran.Text = result.ToString();
-        }
-
-        private void wyczysc_Click(object sender, EventArgs e)
-        {
-            currentInput = "";
-            result = 0;
-            operation = "";
-            operationPending = false;
-            ekran.Text = "";
         }
     }
 }
